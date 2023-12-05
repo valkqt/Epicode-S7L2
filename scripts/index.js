f45a038065184029ae988a16d13b3ecca4bf70b7
Valk <bznand@gmail.com>
2023-12-05 15:02:51 +0100

    birds are hard to catch

---
+++ scripts/index.js
@@ -0,0 +1,50 @@
+class Global {
+    constructor(){
+        this.form = document.querySelector('form')
+        this.inputField = document.getElementById('username')
+        this.resultBox = document.querySelector('[disabled]')
+        this.cancel = document.querySelector('[type=button]')
+        this.counter = document.getElementById('counter')
+    }
+}
+
+
+const global = new Global()
+
+
+function countUp() {
+    window.sessionStorage.setItem('timer', (parseInt(global.counter.textContent) + 1) )
+    global.counter.textContent++
+
+}
+
+function timerStart() {
+    if (window.sessionStorage.getItem('timer')) {
+        global.counter.textContent = window.sessionStorage.getItem('timer')
+    } 
+}
+
+function activateButtons() {
+    global.form.addEventListener('submit', (event) => {
+
+        event.preventDefault()
+    
+        window.localStorage.setItem('username', global.inputField.value)
+        global.resultBox.value = global.inputField.value
+        global.inputField.value = ""
+    
+    })
+    
+    global.cancel.addEventListener('click', () => {
+        if (window.localStorage.getItem('username')) {
+            window.localStorage.removeItem('username')
+            global.resultBox.value = ""
+        }
+    
+    })
+    
+}
+
+activateButtons()
+timerStart()
+window.setInterval(countUp, 1000)
